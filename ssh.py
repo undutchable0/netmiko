6a50e8144ae52ddd1e584d62be033663f11af789
Rens E <toadhop0@gmail.com>
2024-08-16 14:15:56 +0000

    pushed

---
+++ ssh.py
@@ -0,0 +1,32 @@
+from netmiko import ConnectHandler
+import json
+
+sshCli = ConnectHandler(
+    device_type = 'cisco_ios',
+    host = '10.99.2.1',
+    port = 22,
+    username = 'cisco',
+    password = 'cisco'
+)
+
+config_command = [
+    'int lo5',
+    'ip add 1.1.1.1 255.255.255.0',
+    'descrip Change loop'
+    ]
+
+for loop in range(10,15,1):
+    loop_commands = ['int loop ' + str(loop), 'description Looper ' + str(loop)]
+    sshCli.send_config_set(loop_commands)
+
+loopback = sshCli.send_config_set(config_command)
+
+output = sshCli.send_command('show ip int brief', use_textfsm=True)
+
+print(json.dumps(output, indent=2))
+
+
+for interfaces in output:
+    print(interfaces['interface'] , ' is ' , interfaces['status'])
+
+
