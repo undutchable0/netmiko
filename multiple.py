c0bd4fff74836a79a2ee39dbc503604eb0335282
undutchable0 <148560660+undutchable0@users.noreply.github.com>
2024-08-26 15:35:49 -0600

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -17,6 +17,13 @@ for IP in f:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
   tn.write(b"enable\n")
-  tn.write(b"conf t\n")
-  tn.write(b"int loo 200\n")
-  tn.write(b"description HellYah\n")
+  tn.write(b"terminal lenth 0\n")
+  tn.write(b"show run\n")
+  tn.write(b"exit\n")
+  readoutput = tn.read_all()
+  saveoutput = open("switch" = HOST, "w")
+  saveoutput.write(readoutput.decode('ascii'))
+  saveoutput.write("\n")
+  saveoutput.close
+  print(tn.read_all().decode('ascii'))
+ 
