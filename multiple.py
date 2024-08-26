1e016c062075415b5ff4043f7324b7749276c96f
undutchable0 <148560660+undutchable0@users.noreply.github.com>
2024-08-26 15:43:52 -0600

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -1,29 +1,25 @@
 import getpass
 import telnetlib
 
-HOST = "localhost"
+HOST = "10.99.2.1"
 user = input("Enter your remote account: ")
 password = getpass.getpass()
 
-f = open ('hosts.txt')
-
-for IP in f:
-  IP=IP.strip()
-  print ("Configuring Switch " + (IP))
-  tn = telnetlib.Telnet(IP)
-  tn.read_until(b"Username: ")
-  tn.write(user.encode('ascii') + b"\n")
-  if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
-  tn.write(b"enable\n")
-  tn.write(b"terminal lenth 0\n")
-  tn.write(b"show run\n")
-  tn.write(b"exit\n")
-  readoutput = tn.read_all()
-  saveoutput = open("switch" = HOST, "w")
-  saveoutput.write(readoutput.decode('ascii'))
-  saveoutput.write("\n")
-  saveoutput.close
-  print(tn.read_all().decode('ascii'))
+print ("Configuring Switch " + (HOST))
+tn = telnetlib.Telnet(HOST)
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+ if password:
+  tn.read_until(b"Password: ")
+  tn.write(password.encode('ascii') + b"\n")
+tn.write(b"enable\n")
+tn.write(b"terminal lenth 0\n")
+tn.write(b"show run\n")
+tn.write(b"exit\n")
+readoutput = tn.read_all()
+saveoutput = open("switch" + HOST, "w")
+saveoutput.write(readoutput.decode('ascii'))
+saveoutput.write("\n")
+saveoutput.close
+print(tn.read_all().decode('ascii'))
  
