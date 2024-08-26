c40a602dae39a75c481f8a384c8a7450d82e5c63
undutchable0 <148560660+undutchable0@users.noreply.github.com>
2024-08-26 11:37:22 -0600

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -1,7 +1,25 @@
 import getpass
 import telnetlib
 
+HOST = "10.99.2.1"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
 f = open ('hosts.txt')
 
 for IP in f:
-  print(IP)
+  IP=IP.strip()
+  print ("Configuring Switch " + (IP))
+  HOST = IP
+  tn = telnetlib.Telnet(HOST)
+  tn.read_until(b"Username: ")
+  tn.write(user.encode('ascii') + b"\n")
+  if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+  tn.write(b"enable\n")
+  tn.write(b"conf t\n")
+  tn.write(b"int loo 200\n")
+  tn.write(b"description HellYah\n")
+  tn.write(b"end\n")
+  tn.write(b"exit\n")
