388079ae5e63b97f9805d707f7198a1d875da778
undutchable0 <148560660+undutchable0@users.noreply.github.com>
2024-08-27 14:41:19 -0600

    Update ssh2.py

--- ssh2.py
+++ ssh2.py
@@ -20,8 +20,8 @@ all_devices = [SPOKE1, SPOKE2]
 for devices in all_devices:
     net_connect = ConnectHandler(**devices)
     for n in range (2,21):
-       print ("Creating VLAN " + str(n))
-       config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+       print ("Creating LOOPBACK " + str(n))
+       config_commands = ['int loo' + str(n), 'description Loopback ' + str(n)]
        output = net_connect.send_config_set(config_commands)
        print (output)
 
