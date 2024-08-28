d8d4e9ed9050ad3ea3e805b99a955d47c0542a55
undutchable0 <148560660+undutchable0@users.noreply.github.com>
2024-08-28 09:59:57 -0600

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -14,14 +14,15 @@ config_command = [
     'ip add 1.1.1.1 255.255.255.0',
     'descrip HPDM'
     ]
-
+'''
 for loop in range(55,60,1):
     loop_commands = ['int loop ' + str(loop), 'description HPDM ' + str(loop)]
     sshCli.send_config_set(loop_commands)
+'''
 
 loopback = sshCli.send_config_set(config_command)
 
-output = sshCli.send_command('show ip int brief', use_textfsm=True)
+output = sshCli.send_command('show version', use_textfsm=True)
 
 print(json.dumps(output, indent=2))
 
